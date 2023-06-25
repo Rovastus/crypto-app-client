6cd2c602aebcdd0a65139a503803b9045ebb170a
Rovastus <rovastus1@gmail.com>
2023-06-25 13:03:34 +0200

    refactor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,5 @@ export const environment = {
   generateLivecoinwatcTestData: false,
   livecoinwatchApi: 'https://api.livecoinwatch.com/',
   livecoinwatchApiKey: '<API-KEY>',
-  coinImageUrl:
-    'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/',
+  coinImageUrl: 'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  generateLivecoinwatcTestData: false,
+  generateLivecoinwatcTestData: true,
   livecoinwatchApi: 'https://api.livecoinwatch.com/',
-  livecoinwatchApiKey: '4b684707-54a6-4ab5-9117-0e5954483456',
-  coinImageUrl:
-    'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/',
+  livecoinwatchApiKey: '<API-KEY>',
+  coinImageUrl: 'https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/32/',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
