106b8d85b1c98df4979fbb7d6aa86e00dcb347bd
Rovastus <rovastus1@gmail.com>
2023-07-05 08:29:51 +0200

    App component to OnPush strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Signal, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, map, startWith } from 'rxjs';
 import { Link } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
-  navLinks: Link[] = [
+export class AppComponent {
+  private readonly router = inject(Router);
+
+  navLinks: Signal<Link[]> = signal([
     this.createLink('Info', 'info', './info', 0),
     this.createLink('Files', 'note_add', './files', 1),
     this.createLink('Transactions', 'swap_horiz', './transactions', 2),
     this.createLink('Earns', 'savings', './earns', 3),
     this.createLink('Settings', 'settings', './settings', 4),
-  ];
+  ]).asReadonly();
 
-  activeLink: Link = this.navLinks[0];
-
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.router.events.subscribe(() => {
-      const activeLink = this.navLinks.find((tab) => tab.link === '.' + this.router.url);
-      if (activeLink) {
-        this.activeLink = activeLink;
-      }
-    });
-  }
+  activeLink$: Observable<Link> = this.router.events.pipe(
+    startWith(this.navLinks()[0]),
+    map(() => {
+      const activeLink = this.navLinks().find((tab) => tab.link === '.' + this.router.url);
+      return activeLink ? activeLink : this.navLinks()[0];
+    }),
+  );
 
   private createLink(label: string, icon: string, link: string, index: number): Link {
     return { label, icon, link, index };
